81fb80dbc5c5d4673ec4635e18f805d476801c10
HTarzwell <htarzwell@gmail.com>
2018-03-08 18:25:38 -0500

    Added signout function in API.js

--- client/assets/scripts/auth/api.js
+++ client/assets/scripts/auth/api.js
@@ -38,8 +38,20 @@ const changePassword = function (data) {
   })
 }
 
+const signOut = function () {
+  return $.ajax({
+    url: config.apiOrigin + '/sign-out/' + store.user.id,
+    method: 'DELETE',
+    headers: {
+      contentType: 'application/json',
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
 module.exports = {
   signUp,
   signIn,
-  changePassword
+  changePassword,
+  signOut
 }
