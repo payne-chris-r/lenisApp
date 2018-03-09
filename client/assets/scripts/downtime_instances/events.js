80a3e08cad5978a7038121dffbe01352a8c67c9b
HTarzwell <htarzwell@gmail.com>
2018-03-09 12:45:33 -0500

    Added code in full to downtime_instances/events.js

--- client/assets/scripts/downtime_instances/events.js
+++ client/assets/scripts/downtime_instances/events.js
@@ -3,3 +3,20 @@
 const api = require('./api')
 const getFormFields = require('../../../lib/get-form-fields')
 const ui = require('./ui')
+
+const onCreateDowntime = function (event) {
+  event.preventDefault()
+  const data = getFormFields(this)
+  api.createDowntime(data)
+    .then(ui.createDowntimeSuccess)
+    .then($('#user-signup input[type="text"]').val(''))
+    .catch(ui.createDowntimeFailure)
+}
+
+const addHandlers = () => {
+  $('#create-downtime').on('submit', onCreateDowntime)
+}
+
+module.exports = {
+  addHandlers
+}
